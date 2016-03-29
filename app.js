109042edd09de4d9c7b45d86992c886a6ad06a39
GennaroAllocca <gennaroalloccaelis@gmail.com>
2016-03-29 13:18:09 +0100

    Switch to hello jenkins

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hola Gennaro');
+  res.send('hello Jenkins');
 });
 
-app.listen(process.env.PORT || 5000);
+
 
 module.exports = app;
